897a5bc5334f0523153a3e0948852f969b4286db
Sara Baqla <missatrox44@gmail.com>
2022-12-26 17:17:07 -0600

    refactored api url call, login functionality works

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,26 @@
+import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { login } from '../util/auth';
 
 //pass isLogin prop
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  //manage loading state
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  //email/password props already validated in AuthContent.js
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true);
+    await login(email, password);
+    setIsAuthenticating(false);
+  }
+
+  //return overlay while request is on the way
+  if (isAuthenticating) {
+    return <LoadingOverlay message='Logging you in...' />
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
--- util/auth.js
+++ util/auth.js
@@ -1,19 +1,36 @@
 import axios from 'axios';
 
 //obtained API_KEY from firebase project -> settings
+
 const API_KEY = 'AIzaSyCIfoxVboVlMyXbhQIfHpELQGio7jYZZQ4'
 
-//send request to firebase to create user
-//https://firebase.google.com/docs/reference/rest/auth#section-create-email-password
-export async function createUser(email, password) {
-  const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
+//create url dynamically
+export async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url,
     {
       //correctly formatted second argument (object) based on docs
       email: email,
       password: password,
       returnSecureToken: true
-    }
-  );
+    });
+
+  //tell if request sent successfully
+  console.log(response.data);
 }
+
+//send request to firebase to create user
+//https://firebase.google.com/docs/reference/rest/auth#section-create-email-password
+export async function createUser(email, password) {
+  //mode spelled just like in docs
+  await authenticate('signUp', email, password)
+}
+
+
+export async function login(email, password) {
+  await authenticate('signInWithPassword', email, password)
+}
+
 
 //use async await to ensure that function will return a promise that will resolve once request is done

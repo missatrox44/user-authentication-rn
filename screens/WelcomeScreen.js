9a5ec194aad840e8f6fe2dcd4705ba0ee53cbaa6
Sara Baqla <missatrox44@gmail.com>
2023-01-02 17:39:07 -0600

    access protected resource

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,11 +1,31 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
+
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 //welcome screen only available to view after user has logged in
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState('');
+
+  //import context to access token value
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+  //accessing 'protected resources'
+  //demo 'protected message' data 
+  //add ?auth= as query param for firebase API calls
+  useEffect(() => {
+    axios.get('https://user-authentication-rn-default-rtdb.firebaseio.com/message.json?auth=' + token)
+      .then((response) => {
+        setFetchedMessage(response.data)
+      });
+  }, []);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
